5fd33369fbe735c41ee59bb5cf175cc1b893ae9a
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Pro.local>
2025-03-29 21:26:23 -0500

    added logger

--- process_data.py
+++ process_data.py
@@ -1,9 +1,12 @@
 import utils
+from logger import app_logger
 
 if __name__ == "__main__":
+    app_logger.info("Starting data processing script")
     config = utils.load_config()
     session = utils.assume_role(config["aws_role"])
     
+    app_logger.info("Fetching data from Athena tables")
     dataframes = utils.read_athena_tables(
         config["query_files"], 
         config["source_database"], 
@@ -13,6 +16,7 @@ if __name__ == "__main__":
     
     for i, df in enumerate(dataframes):
         if not df.empty:
+            app_logger.info(f"Processing dataframe {i}")
             df["new_column"] = df["existing_column"] * 2  # Example transformation
             utils.write_to_s3(
                 df, 
@@ -21,5 +25,6 @@ if __name__ == "__main__":
                 file_format=config["file_format"], 
                 session=session
             )
+            app_logger.info(f"Successfully wrote processed_data_{i}.{config["file_format"]} to S3")
     
-    print("Data processing complete!")
+    app_logger.info("Data processing complete!")
